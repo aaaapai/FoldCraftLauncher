39857e453c085d64ab76719dd98d319b2a8dd93c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-28 22:49:03 +0800

    Update build.gradle.kts

--- FCLCore/build.gradle.kts
+++ FCLCore/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
         }
         create("fordebug") {
             initWith(getByName("debug"))
@@ -25,12 +25,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
 }
 
@@ -39,13 +39,13 @@ dependencies {
     implementation(project(":ZipFileSystem"))
     implementation("org.nanohttpd:nanohttpd:2.3.1")
     implementation("com.github.steveice10:opennbt:1.5")
-    implementation("org.tukaani:xz:1.9")
-    implementation("commons-io:commons-io:2.15.1")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-compress:1.26.0")
-    implementation("com.moandjiezana.toml:toml4j:0.7.2")
+    implementation("org.tukaani:xz:1.10")
+    implementation("commons-io:commons-io:2.19.0")
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    implementation("org.apache.commons:commons-compress:1.27.1")
+    implementation("io.hotmoka:toml4j:0.7.3")
     implementation("org.jenkins-ci:constant-pool-scanner:1.2")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0-alpha13")
 }
