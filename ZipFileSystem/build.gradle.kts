364cb6224b43699fe27348e86925cf503e9ca3aa
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-04-28 22:24:16 +0800

    Add files via upload

--- ZipFileSystem/build.gradle.kts
+++ ZipFileSystem/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
         }
         create("fordebug") {
             initWith(getByName("debug"))
@@ -24,7 +24,7 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
