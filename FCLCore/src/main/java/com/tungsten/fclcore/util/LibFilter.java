bcf598489aa22258dbe19c2e3324e5f8747c0552
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-01 14:15:16 +0800

    Add files via upload

--- FCLCore/src/main/java/com/tungsten/fclcore/util/LibFilter.java
+++ FCLCore/src/main/java/com/tungsten/fclcore/util/LibFilter.java
@@ -10,40 +10,51 @@ import java.util.List;
 
 public class LibFilter {
 
-    private static final String ASM_ALL_5_0_4_STRING = "{\n" +
-            "      \"name\": \"org.ow2.asm:asm-all:5.0.4\",\n" +
+    private static final String ASM_ALL_5_2_STRING = "{\n" +
+            "      \"name\": \"org.ow2.asm:asm-all:5.2\",\n" +
             "      \"downloads\": {\n" +
             "        \"artifact\": {\n" +
-            "          \"path\": \"org/ow2/asm/asm-all/5.0.4/asm-all-5.0.4.jar\",\n" +
-            "          \"sha1\": \"e6244859997b3d4237a552669279780876228909\",\n" +
-            "          \"url\": \"https://repo1.maven.org/maven2/org/ow2/asm/asm-all/5.0.4/asm-all-5.0.4.jar\"\n" +
+            "          \"path\": \"org/ow2/asm/asm-all/5.2/asm-all-5.2.jar\",\n" +
+            "          \"sha1\": \"2ea49e08b876bbd33e0a7ce75c8f371d29e1f10a\",\n" +
+            "          \"url\": \"https://repo1.maven.org/maven2/org/ow2/asm/asm-all/5.2/asm-all-5.2.jar\"\n" +
             "        }\n" +
             "      }\n" +
             "    }";
-    private static final String JNA_5_13_STRING = "{\n" +
-            "      \"name\": \"net.java.dev.jna:jna:5.13.0\",\n" +
+    private static final String JNA_5_17_STRING = "{\n" +
+            "      \"name\": \"net.java.dev.jna:jna:5.17.0\",\n" +
             "      \"downloads\": {\n" +
             "        \"artifact\": {\n" +
-            "          \"path\": \"net/java/dev/jna/jna/5.13.0/jna-5.13.0.jar\",\n" +
-            "          \"sha1\": \"1200e7ebeedbe0d10062093f32925a912020e747\",\n" +
-            "          \"url\": \"https://repo1.maven.org/maven2/net/java/dev/jna/jna/5.13.0/jna-5.13.0.jar\"\n" +
+            "          \"path\": \"net/java/dev/jna/jna-jpms/5.17.0/jna-jpms-5.17.0.jar\",\n" +
+            "          \"sha1\": \"5dc5682b4228c7efc0741fa12606ea4d3983a9e4\",\n" +
+            "          \"url\": \"https://repo1.maven.org/maven2/net/java/dev/jna/jna-jpms/5.17.0/jna-jpms-5.17.0.jar\"\n" +
             "        }\n" +
             "      }\n" +
             "    }";
-    private static final String OSHI_6_3_STRING = "{\n" +
-            "      \"name\": \"com.github.oshi:oshi-core:6.3.0\",\n" +
+    private static final String JNA_5_17_PLATFORM_STRING = "{\n" +
+            "      \"name\": \"net.java.dev.jna:jna:5.17.0\",\n" +
             "      \"downloads\": {\n" +
             "        \"artifact\": {\n" +
-            "          \"path\": \"com/github/oshi/oshi-core/6.3.0/oshi-core-6.3.0.jar\",\n" +
-            "          \"sha1\": \"9e98cf55be371cafdb9c70c35d04ec2a8c2b42ac\",\n" +
-            "          \"url\": \"https://repo1.maven.org/maven2/com/github/oshi/oshi-core/6.3.0/oshi-core-6.3.0.jar\"\n" +
+            "          \"path\": \"net/java/dev/jna/jna-platform-jpms/5.17.0/jna-platform-jpms-5.17.0.jar\",\n" +
+            "          \"sha1\": \"3e188fef07cb6a9ac434ec30f59e6c622045bf13\",\n" +
+            "          \"url\": \"https://repo1.maven.org/maven2/net/java/dev/jna/jna-platform-jpms/5.17.0/jna-platform-jpms-5.17.0.jar\"\n" +
+            "        }\n" +
+            "      }\n" +
+            "    }";
+    private static final String OSHI_6_8_1_STRING = "{\n" +
+            "      \"name\": \"com.github.oshi:oshi-core:6.8.1\",\n" +
+            "      \"downloads\": {\n" +
+            "        \"artifact\": {\n" +
+            "          \"path\": \"com/github/oshi/oshi-core/6.8.1/oshi-core-6.8.1.jar\",\n" +
+            "          \"sha1\": \"dfd7f288123f92053c546373276100ff1c3302f5\",\n" +
+            "          \"url\": \"https://repo1.maven.org/maven2/com/github/oshi/oshi-core/6.8.1/oshi-core-6.8.1.jar\"\n" +
             "        }\n" +
             "      }\n" +
             "    }";
 
-    private static final Library ASM_ALL_5_0_4 = GSON.fromJson(ASM_ALL_5_0_4_STRING, Library.class);
-    private static final Library JNA_5_13 = GSON.fromJson(JNA_5_13_STRING, Library.class);
-    private static final Library OSHI_6_3 = GSON.fromJson(OSHI_6_3_STRING, Library.class);
+    private static final Library ASM_ALL_5_2 = GSON.fromJson(ASM_ALL_5_2_STRING, Library.class);
+    private static final Library JNA_5_17 = GSON.fromJson(JNA_5_17_STRING, Library.class);
+    private static final Library JNA_5_17_PLATFORM = GSON.fromJson(JNA_5_17_PLATFORM_STRING, Library.class);
+    private static final Library OSHI_6_8_1 = GSON.fromJson(OSHI_6_8_1_STRING, Library.class);
 
     public static Version filter(Version version) {
         return version.setLibraries(filterLibs(version.getLibraries()));
@@ -54,21 +65,15 @@ public class LibFilter {
         for (Library library : libraries) {
             if (!library.getName().contains("org.lwjgl") && !library.getName().contains("jinput-platform") && !library.getName().contains("twitch-platform")) {
                 String[] version = library.getName().split(":")[2].split("\\.");
-                if (library.getArtifactId().equals("asm-all") && Integer.parseInt(version[0]) < 5) {
-                    newLibraries.add(ASM_ALL_5_0_4);
+                if (library.getArtifactId().equals("asm-all") {
+                    newLibraries.add(ASM_ALL_5_2);
                 } else if (library.getName().startsWith("net.java.dev.jna:jna:")) {
-                    if (Integer.parseInt(version[0]) >= 5 && Integer.parseInt(version[1]) >= 13) {
-                        newLibraries.add(library);
-                    } else {
-                        newLibraries.add(JNA_5_13);
-                    }
+                        newLibraries.add(JNA_5_17);
+                } else if (library.getName().startsWith("net.java.dev.jna:jna-platform:")) {
+                        newLibraries.add(JNA_5_17_PLATFORM);
                 } else if (library.getName().startsWith("com.github.oshi:oshi-core:")) {
-                    if (Integer.parseInt(version[0]) != 6 || Integer.parseInt(version[1]) != 2) {
-                        newLibraries.add(library);
-                    } else {
-                        newLibraries.add(OSHI_6_3);
-                    }
-                } else {
+                        newLibraries.add(OSHI_6_8_1);
+                   } else {
                     newLibraries.add(library);
                 }
             }
