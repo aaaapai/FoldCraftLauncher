587e8eefbe94a3727e49a97f764f1be412a19b06
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-01 17:30:03 +0800

    Update build.gradle.kts

--- ZipFileSystem/build.gradle.kts
+++ ZipFileSystem/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled true
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
