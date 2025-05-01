c8420a8418515e2f649841c0674d480da3fd450c
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-01 17:25:15 +0800

    Update build.gradle.kts

--- FCLauncher/build.gradle.kts
+++ FCLauncher/build.gradle.kts
@@ -21,8 +21,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
-            zipAlignEnabled = true
-            shrinkResources = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
