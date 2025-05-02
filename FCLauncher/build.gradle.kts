4bf3fdb97682c512d8514f4c173a00782da5985e
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-05-02 19:34:56 +0800

    Update build.gradle.kts

--- FCLauncher/build.gradle.kts
+++ FCLauncher/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 }
 
 dependencies {
-    implementation(fileTree("dir" to "libs", "include" to listOf("*.aar")))
+    implementation(name = "bytehook-release", ext = "aar")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.13.0-alpha13")
 }
